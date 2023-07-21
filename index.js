c58e685b6025cabf627bb631b8d8969df67bdab3
Christin-paige <christinpmartin@gmail.com>
2023-07-21 08:27:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
+// GET requests
+app.get('/', (req, res) => {
+    res.send('MyFlix: All of the Movies Worth Caring About');
+  });
+  
+  app.get('/documentation', (req, res) => {                  
+    res.sendFile('public/documentation.html', { root: __dirname });
+  });
+  
+  app.get('/movies', (req, res) => {
+    res.json(topTenMovies);
+  });
+
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
