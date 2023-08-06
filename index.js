654f823ecf86d3a5d7207ab0fdb2d3f4a99b36de
Christin-paige <christinpmartin@gmail.com>
2023-08-06 08:00:56 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express'),
+const express = require("express");
 
-uuid = require('uuid');
+uuid = require("uuid");
 
-const morgan = require('morgan');
+const morgan = require("morgan");
 const app = express();
-const mongoose = require('mongoose');
-const Models = require('./models.js');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+const bodyParser = require("body-parser");
 
-fs = require('fs'),
-path = require('path')
+fs = require("fs"),
+path = require("path")
 app.use(express.json()),
 app.use(express.urlencoded({
   extended: true
@@ -18,26 +18,26 @@ app.use(express.urlencoded({
 
 const Movies = Models.Movie;
 const Users = Models.User;
-//const Genres = Models.Genre;
+const Genres = Models.Genre;
 //const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/[myflixdb]',
+mongoose.connect("mongodb://localhost:27017/[myflixdb]",
 { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("common"));
 
 // GET requests
 app.get('/', (req, res) => {
-  res.send('MyFlix: All of the Movies Worth Caring About');
+  res.send("MyFlix: All of the Movies Worth Caring About");
 });
 
-app.get('/documentation', (req, res) => {                  
-  res.sendFile('public/documentation.html', { root: __dirname });
+app.get("/documentation", (req, res) => {                  
+  res.sendFile("public/documentation.html", { root: __dirname });
 });
-
-app.get('/movies', (req, res) => {
+//get all movies
+app.get("/movies", (req, res) => {
   Movies.find()
   .then((movies) => {
     res.status(201).json(movies);
@@ -50,73 +50,127 @@ app.get('/movies', (req, res) => {
 
 
 //Get all users
-app.get('/users', async (req, res) =>  {
+app.get("/users", async (req, res) =>  {
   await Users.find()
   .then((users) => {
     res.status(201).json(users);
   })
   .catch((err) => {
     console.error(err);
-    res.status(500).send('Error: ' + err);
+    res.status(500).send("Error: " + err);
   });
 });
 
 //Get JSON movie info when looking for a specific title
-app.get('/movies/:Title', (req, res) => {
+app.get("/movies/:Title", (req, res) => {
   Movies.findOne({ Title: req.params.Title })
   .then ((movie) => {
     res.json(movie);
   })
   .catch((err) => {
     console.error(err);
-    res.status(500).send('Error: ' + err);
+    res.status(500).send("Error: " + err);
   });
 });
 
 //CREATE
-app.post('/users', async (req, res) => {
-  await Users.findOne({Username: req.body.Username })
+app.post("/users", async (req, res) => {
+  await Users.findOne({Name: req.body.Name })
   .then((user) => {
     if (user) {
-      return res.status(400).send(req.body.Username + 'already exists');
+      return res.status(400).send(req.body.Name + "already exists");
     } else {
       Users
       .create ({
-        Username: req.body.Username,
-        Password: req.body.Password,
+        Name: req.body.Username,
         Email: req.body.Email,
-        Birthday: req.body.Birthday
+        Password: req.body.Password,
+        Birthday: req.body.Birthday,
+        FavoriteMovies: req.body.FavoriteMovies
       })
       .then((user) => {res.status(201).json(user) })
       .catch((error) => {
         console.error(error);
-        res.status(500).send('Error: ' + error);
+        res.status(500).send("Error: " + error);
       })
     }
   })
   .catch((error) => {
     console.error(error);
-    res.status(500).send('Error: ' + error);
+    res.status(500).send("Error: " + error);
   })
 });
 
 //Get a user by name
-app.get('/users/:Username', async (req, res) => {
-  await Users.findOne({ Username: req.params.Username })
+app.get("/users/:Name", async (req, res) => {
+  await Users.findOne({ Name: req.params.Name })
   .then((user) => {
     res.json(user);
   })
   .catch((err) => {
     console.error(err);
-    res.status(500).send('Error: ' + err);
+    res.status(500).send("Error: " + err);
+  });
+});
+
+//update user info, by username
+app.put("/users/:Name", async (req, res) => {
+  await Users.findOneAndUpdate ({ Username: req.params.Name }, 
+    { 
+      $set: {
+  Name: req.body.Username,
+  Password: req.body.Password,
+  Email: req.body.Email,
+  Birthday: req.body.Birthday,
+  FavoriteMovies: req.body.FavoriteMovies
+}
+},
+{ new: true }  )
+.then((updatedUser) => {
+  res.json(updatedUser);
+})
+.catch((err) => {
+  console.error(err);
+  res.status(500).send("Error: " + err);
+})
+});
+
+//add a movie to a user's list of favorites
+app.post("/users/:Name/movies/:MovieID", async (req, res) => {
+  await Users.findOneAndUpdate({ Name: req.params.Name }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
+
+//Delete user
+app.delete("/users/:Name", (req, res)=> {
+  Users.findOneAndRemove ({ Name: req.params.Name})
+  .then((user) => {
+    if (!user) {
+      res.status(400).send(req.params.Name + " was not found");
+
+    }else {
+      res.status(200).send(req.params.Name + " was deleted.");
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
   });
 });
 
 
 
-
-
 /*
+
 //UPDATE
 app.put('/users/:id', (req,res) => {
   const { id } = req.params;
@@ -220,20 +274,19 @@ app.get('/movies/genre/:genreName', (req, res) => {
 
 });
 
+
 */
 
-
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'),
-{flags: 'a'})
-app.use(morgan('combined', {stream: accessLogStream}));
-app.use(morgan('common'));
-
-app.use(express.static('public'));
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"),
+{flags: "a"})
+app.use(morgan("combined", {stream: accessLogStream}));
+app.use(morgan("common"));
+app.use(express.static("public"));
 
 
 
   app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
+    console.log("Your app is listening on port 8080.");
   });
 
   
--- models.js
+++ models.js
@@ -14,15 +14,16 @@ let movieSchema = mongoose.Schema ({
     });
 
     let userSchema = mongoose.Schema ({
-        Username: {type: String, required: true},
-        Password: {type: String, required: true},
+        Name: {type: String, required: true},
         Email: {type: String, required: true},
+        Password: {type: String, required: true},
         Birthday: Date,
         FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
     });
 
     let Movie = mongoose.model('Movie', movieSchema);
     let User = mongoose.model('User', userSchema);
+    
 
     module.exports.Movie = Movie;
     module.exports.User = User;
