e17d5e6637b26529880b8fca2652cb77b89ca072
Christin-paige <christinpmartin@gmail.com>
2023-08-09 13:21:55 -0400

    index.js
    debugging, cannot POST user in postman

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 app.use(cors());
+
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
