d6bf6f5db7ab04536a50a0c99f647b3822e3d135
Christin-paige <christinpmartin@gmail.com>
2023-09-03 19:00:14 -0400

    changed router to method in auth.js

--- auth.js
+++ auth.js
@@ -34,7 +34,7 @@ module.exports = (router) => {
         }) (req, res);
     });
 
-    router.post('/login', (req, res)=> {
+    method.post('/login', (req, res)=> {
         res.send('POST request to /login');
     })
 }
